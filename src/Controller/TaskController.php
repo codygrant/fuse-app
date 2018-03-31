55618f3109d1d4b0859dc0be9100e260b27a9f2b
Cody Grant <codygrant0324@gmail.com>
2018-03-31 12:02:26 -0400

    Add EntityManager to TaskController and query + send tasks to view

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -8,17 +8,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class TaskController extends Controller {
 
     private $session;
+    private $em;
 
-    public function __construct(SessionInterface $session) {
+    public function __construct(SessionInterface $session, EntityManagerInterface $em) {
         $this->session = $session;
+        $this->em = $em;
     }
 
     /**
@@ -26,6 +30,7 @@ class TaskController extends Controller {
      * @Route("/")
      */
     public function index() {
-        return $this->render('home.html.twig');
+        $tasks = $this->em->getRepository(Task::class)->findAll();
+        return $this->render('home.html.twig', array('tasks' => $tasks));
     }
 }
