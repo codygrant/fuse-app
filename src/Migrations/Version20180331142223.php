ec930189127d7793806f97ce49227c04c19c97ea
Cody Grant <codygrant0324@gmail.com>
2018-03-31 10:27:33 -0400

    Add file to track DB migrations

---
+++ src/Migrations/Version20180331142223.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180331142223 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE task (id INT AUTO_INCREMENT NOT NULL, source VARCHAR(100) NOT NULL, project VARCHAR(100) NOT NULL, site_name VARCHAR(100) NOT NULL, title VARCHAR(100) NOT NULL, list VARCHAR(100) NOT NULL, url VARCHAR(100) NOT NULL, status VARCHAR(100) NOT NULL, description VARCHAR(100) NOT NULL, due_date DATETIME NOT NULL, last_updated DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE task');
+    }
+}
