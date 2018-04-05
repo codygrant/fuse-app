29a1604be5f3962a6ae6bc10eaa9de7a9af3ccc5
Cody Grant <codygrant0324@gmail.com>
2018-04-04 21:46:32 -0400

    Create function to update a Task in DB
    When populating cards it will now update a card if it the activity
    has since changed since it was last synced. Before it would recreate
    a new entry if the date changed.

--- src/Utils/TrelloServices.php
+++ src/Utils/TrelloServices.php
@@ -46,30 +46,52 @@ class TrelloServices {
                 'task_id' => $card['id'],
             ]);
 
-            // don't update card if activity hasn't changed
-            $card_activity = new DateTime($card['dateLastActivity']);
-            if ($entity != null && $card_activity <= $entity->getLastUpdated())
-                continue;
-
             // use API to get board, list name from ID
             $list = $this->client->api('lists')->show($card['idList']);
             $project = $this->client->api('boards')->show($card['idBoard']);
 
-            $task = new Task();
-            $task->setTaskId($card['id']);
-            $task->setSource('trello');
-            $task->setProject($project['name']);
-            $task->setTitle($card['name']);
-            $task->setList($list['name']);
-            $task->setUrl($card['shortUrl']);
-            $task->setDescription($card['desc']);
-            $task->setLastUpdated(new DateTime($card['dateLastActivity']));
-            if ($card['due'] != null)
-                $task->setDueDate(new DateTime($card['due']));
+            // don't update card if activity hasn't changed
+            $card_activity = new DateTime($card['dateLastActivity']);
+            if ($entity && $card_activity >= $entity->getLastUpdated()) {
+                $this->updateTask($entity, $card, $list, $project);
+            }
+            else {
+                $task = new Task();
+                $task->setTaskId($card['id']);
+                $task->setSource('trello');
+                $task->setProject($project['name']);
+                $task->setTitle($card['name']);
+                $task->setList($list['name']);
+                $task->setUrl($card['shortUrl']);
+                $task->setDescription($card['desc']);
+                $task->setLastUpdated(new DateTime($card['dateLastActivity']));
+                if ($card['due'] != null)
+                    $task->setDueDate(new DateTime($card['due']));
 
-            // save to db
-            $this->em->persist($task);
-            $this->em->flush();
+                // save to db
+                $this->em->persist($task);
+                $this->em->flush();
+            }
         }
+    }
+
+    /**
+     * @param Task $task
+     * @param $card
+     * @param $list
+     * @param $project
+     */
+    public function updateTask($task, $card, $list, $project) {
+        $task->setTaskId($card['id']);
+        $task->setSource('trello');
+        $task->setProject($project['name']);
+        $task->setTitle($card['name']);
+        $task->setList($list['name']);
+        $task->setUrl($card['shortUrl']);
+        $task->setDescription($card['desc']);
+        $task->setLastUpdated(new DateTime($card['dateLastActivity']));
+        if ($card['due'] != null)
+            $task->setDueDate(new DateTime($card['due']));
+        $this->em->flush();
     }
 }
