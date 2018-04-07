31f645fc2e6c740a90df67bd3c083c58f4c18f87
Cody Grant <codygrant0324@gmail.com>
2018-04-07 11:41:37 -0400

    Refactor code for building a Task object
    Removed updateCard() and created function to build a Task that gets
    called when updating a card and creating a new card.
    
    Also fixed issue with else block creating a new Task to DB when it
    should be skipped. Now will only create new entry if no match found
    in DB.

--- src/Utils/TrelloServices.php
+++ src/Utils/TrelloServices.php
@@ -37,60 +37,44 @@ class TrelloServices
         // get all cards
         $cards = $this->client->api('member')->cards()->all($resource_owner->nickname);
 
-        foreach ($cards as $card) {
+        foreach ($cards as $current_card) {
             // pull matching card from DB and check date of last activity
             $repository = $this->em->getRepository(Task::class);
             $entity = $repository->findOneBy([
                 'source' => 'trello',
-                'task_id' => $card['id'],
+                'task_id' => $current_card['id'],
             ]);
 
-            // use API to get board, list name from ID
-            $list = $this->client->api('lists')->show($card['idList']);
-            $project = $this->client->api('boards')->show($card['idBoard']);
-
             // don't update card if activity hasn't changed
-            $card_activity = new DateTime($card['dateLastActivity']);
-            if ($entity && $card_activity >= $entity->getLastUpdated()) {
-                $this->updateTask($entity, $card, $list, $project);
-            } else {
-                $task = new Task();
-                $task->setTaskId($card['id']);
-                $task->setSource('trello');
-                $task->setProject($project['name']);
-                $task->setTitle($card['name']);
-                $task->setList($list['name']);
-                $task->setUrl($card['shortUrl']);
-                $task->setDescription($card['desc']);
-                $task->setLastUpdated(new DateTime($card['dateLastActivity']));
-                if ($card['due'] != null) {
-                    $task->setDueDate(new DateTime($card['due']));
-                }
+            $card_activity = new DateTime($current_card['dateLastActivity']);
+            if ($entity && $card_activity > $entity->getLastUpdated()) {
+                $this->buildTask($entity, $current_card);
+                $this->em->flush();
+            } elseif (!$entity) {
+                $current_task = new Task();
+                $this->buildTask($current_task, $current_card);
                 // save to db
-                $this->em->persist($task);
+                $this->em->persist($current_task);
                 $this->em->flush();
             }
         }
     }
 
-    /**
-     * @param Task $task
-     * @param $card
-     * @param $list
-     * @param $project
-     */
-    public function updateTask($task, $card, $list, $project) {
-        $task->setTaskId($card['id']);
-        $task->setSource('trello');
-        $task->setProject($project['name']);
-        $task->setTitle($card['name']);
-        $task->setList($list['name']);
-        $task->setUrl($card['shortUrl']);
-        $task->setDescription($card['desc']);
-        $task->setLastUpdated(new DateTime($card['dateLastActivity']));
-        if ($card['due'] != null) {
-            $task->setDueDate(new DateTime($card['due']));
+    public function buildTask(Task $current_task, $current_card) {
+        // use API to get board, list name from ID
+        $card_list = $this->client->api('lists')->show($current_card['idList']);
+        $project = $this->client->api('boards')->show($current_card['idBoard']);
+
+        $current_task->setTaskId($current_card['id']);
+        $current_task->setSource('trello');
+        $current_task->setProject($project['name']);
+        $current_task->setTitle($current_card['name']);
+        $current_task->setList($card_list['name']);
+        $current_task->setUrl($current_card['shortUrl']);
+        $current_task->setDescription($current_card['desc']);
+        $current_task->setLastUpdated(new DateTime($current_card['dateLastActivity']));
+        if ($current_card['due'] != null) {
+            $current_task->setDueDate(new DateTime($current_card['due']));
         }
-        $this->em->flush();
     }
 }
