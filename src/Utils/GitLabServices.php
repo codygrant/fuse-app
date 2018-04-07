8b723d61d6ef6bd04d17adf51c146d23820f3d83
Cody Grant <codygrant0324@gmail.com>
2018-04-07 13:01:28 -0400

    Create base class for GitLabServices

---
+++ src/Utils/GitLabServices.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: codygrant
+ * Date: 2018-04-07
+ * Time: 12:58 PM
+ */
+
+namespace App\Utils;
+
+use App\Entity\Task;
+use Gitlab\Client;
+use Doctrine\ORM\EntityManagerInterface;
+use DateTime;
+
+class GitLabServices
+{
+
+}
